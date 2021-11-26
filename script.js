edbb686b4a0861ca7c7ceef17da1c84c2fe4e849
funkosaur <mario.bojkovski@outlook.com>
2021-11-26 01:56:20 +0000

    almost dome

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 const gameBoard = (() => {
-  const topLeft = document.querySelector("#top-left");
-  const topMiddle = document.querySelector("#top-middle");
-  const topRight = document.querySelector("#top-right");
-  const middleLeft = document.querySelector("#middle-left");
-  const middleCenter = document.querySelector("#middle-center");
-  const middleRight = document.querySelector("#middle-right");
-  const bottomLeft = document.querySelector("#bottom-left");
-  const bottomMiddle = document.querySelector("#bottom-middle");
-  const bottomRight = document.querySelector("#bottom-right");
   const allGameFields = document.querySelectorAll(".board");
   const allGameFieldsArray = Array.from(allGameFields);
   const startButton = document.querySelector(".start");
-  const gameBoardDiv = document.querySelector(".game-board-div");
   const startAgainButton = document.querySelector(".start-again");
+  const playerOneScore = document.querySelector("#score1");
+  const playerTwoScore = document.querySelector("#score2");
 
 
   let gameBoardArray = ["0", "1", "2",
@@ -30,31 +22,29 @@ const gameBoard = (() => {
 
     if(this.textContent == "X" || this.textContent == "O") return
 
-    if(playerOne.turn == 0){
-      sign = "O"
-    } else { sign = "X" }
 
     let selectedField = allGameFieldsArray.indexOf(this);
 
-    gameBoardArray[selectedField] = sign;
-
-    this.textContent = sign;
+    if(playerOne.turn == 0){
+      gameBoardArray[selectedField] = playerTwo.sign;
+      this.textContent = playerTwo.sign;
+    } else { 
+      gameBoardArray[selectedField] = playerOne.sign;
+      this.textContent = playerOne.sign; 
+    }
 
     if(playerOne.turn == 0){
        playerOne.turn = 1
     } else { playerOne.turn = 0} 
 
     gameFunction()
+
+    if(this.style.backgroundColor == "green") return
+
+    displayController.whoseTurnIsIt ()
     
   }
 
-  function whenGameWon ( originalField, firstField, secondField) {
-    originalField.style.backgroundColor = "green";                                                                           
-    firstField.style.backgroundColor = "green";                                                                                
-    secondField.style.backgroundColor = "green";
-    startAgainButton.style.display = "block";
-    
-  }
 
 
 
@@ -64,22 +54,22 @@ const gameBoard = (() => {
 
     if(index == 0 || index == 3 || index == 6) {
       if(gameBoardArray[index + 1] == field && gameBoardArray[index + 2] == field){ 
-      whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 1], allGameFieldsArray[index + 2])}
+      displayController.whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 1], allGameFieldsArray[index + 2])}
     }
 
     if(index == 0 || index == 1 || index == 2) {
       if(gameBoardArray[index + 3] == field && gameBoardArray[index + 6] == field){ 
-        whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 3], allGameFieldsArray[index + 6])}
+        displayController.whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 3], allGameFieldsArray[index + 6])}
     }
 
     if(index == 0) {
       if(gameBoardArray[index + 4] == field && gameBoardArray[index + 8] == field){ 
-        whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 4], allGameFieldsArray[index + 8])}
+        displayController.whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 4], allGameFieldsArray[index + 8])}
     }
 
     if(index == 2) {
       if(gameBoardArray[index + 2] == field && gameBoardArray[index + 4] == field){ 
-        whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 2], allGameFieldsArray[index + 4])}
+        displayController.whenGameWon(allGameFieldsArray[index], allGameFieldsArray[index + 2], allGameFieldsArray[index + 4])}
     }
   })
 
@@ -95,7 +85,14 @@ const gameBoard = (() => {
                      "3", "4", "5",
                      "6", "7", "8"];
 
-   startAgainButton.style.display = "none"
+   startAgainButton.style.display = "none";
+
+   let randomNumber = Math.floor(Math.random() * 2);
+   
+   if (randomNumber == 0) playerOne.turn = 0;
+   if (randomNumber == 1) playerOne.turn = 1;
+
+   displayController.whoseTurnIsIt ()
 
  }
 
@@ -105,30 +102,29 @@ const gameBoard = (() => {
 
 
 
-  //staj im sign na playerone two i computer za da znaeme koj pobedil
+  //audio ushte da napraish da komentirash i mozebi computer da napraish
 
   
 
-
+ return {startAgain}
 
 })();
 
 const playerOne = (() => {
   let turn = 1;
+  let sign = "X";
+  let score = 0;
   
 
 
-  return {turn}
+  return {turn, sign, score}
 })();
 
 const playerTwo = (() => {
+let sign = "O"
+let score = 0;
 
-  let sign
-
-  if(playerOne.sign == "X"){ sign = "O"}
-  else { sign = "X" }
-
-
+return{sign, score}
 })();
 
 const computerOne = (() => {
@@ -142,10 +138,14 @@ const displayController = (() => {
   
   const startButton = document.querySelector(".start");
   const gameBoardDiv = document.querySelector(".game-board-div");
+  const allGameFields = document.querySelectorAll(".board");
   const choosePlayersDiv = document.querySelector(".choose-players");
   const gameState = document.querySelector(".game-state");
   const chooseOneToPlay = document.querySelector("#choose-one");
-  const textForChoosing = document.querySelector("#label-choose")
+  const textForChoosing = document.querySelector("#label-choose");
+  const playerOneScore = document.querySelector("#score1");
+  const playerTwoScore = document.querySelector("#score2");
+  const startAgainButton = document.querySelector(".start-again");
 
   
 
@@ -164,7 +164,30 @@ const displayController = (() => {
     choosePlayersDiv.style.display = "none";
     gameState.style.display = "block";
     setTimeout(function(){ gameState.style.display = "none" }, 3000);
-  })
+    whoseTurnIsIt ()
+  });
+
+  function whenGameWon ( originalField, firstField, secondField) {
+    originalField.style.backgroundColor = "green";                                                                           
+    firstField.style.backgroundColor = "green";                                                                                
+    secondField.style.backgroundColor = "green";
+    startAgainButton.style.display = "block";
+    console.log(originalField.textContent)
+    if(originalField.textContent == "X") playerOne.score += 1 ;
+    if(originalField.textContent == "O") playerTwo.score += 1;
+    playerOneScore.textContent = playerOne.score;
+    playerTwoScore.textContent = playerTwo.score;
+  }
+
+  function whoseTurnIsIt () {
+    
+    if(playerOne.turn == 1) {playerOneScore.style.boxShadow = "0px 0px 5px #f8f9fa"}
+    else{playerOneScore.style.boxShadow = ""}
+    if(playerOne.turn == 0) {playerTwoScore.style.boxShadow = "0px 0px 5px #f8f9fa"} 
+    else{playerTwoScore.style.boxShadow = ""}
+  }
+
+  return {whenGameWon, whoseTurnIsIt}
 
 
 })();
